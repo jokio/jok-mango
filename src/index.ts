caca2b9dc6920627045871153d6e2e7990323dbc
Ezeki Zibzibadze <ez@jok.io>
2019-02-28 17:23:47 +0400

    fix: expose getClient and getRepository functions

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from './types'
 export { mapKeysToItems } from './loaders/mapKeysToItems'
+export { default as getClient } from './common/getClient'
+export { default as getRepository } from './common/getRepository'
