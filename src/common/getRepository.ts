1232d858650dab6f11ade667b3d10a56858d491d
Ezeki Zibzibadze <ez@jok.io>
2019-02-28 19:21:23 +0400

    fix: add missing delete function in repository

--- src/common/getRepository.ts
+++ src/common/getRepository.ts
@@ -1,6 +1,7 @@
 import { Db } from 'mongodb'
 import collectionFn from '../operations/collection'
 import createFn from '../operations/create'
+import deleteFn from '../operations/delete'
 import getFn from '../operations/get'
 import findFn from '../operations/query'
 import updateFn from '../operations/update'
@@ -16,6 +17,7 @@ export default function <TDocument extends DocumentBase>(
 		get: getFn<TDocument>(db, collectionName, options),
 		query: findFn<TDocument>(db, collectionName, options),
 		update: updateFn<TDocument>(db, collectionName, options),
+		delete: deleteFn<TDocument>(db, collectionName, options),
 		collection: collectionFn<TDocument>(db, collectionName, options),
 	}
 }
