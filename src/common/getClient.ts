36b969f649de5031de87e70ee5b023d38197df6a
Ezeki Zibzibadze <ez@jok.io>
2019-11-13 20:42:03 +0400

    feat: add poolSize arg for getClient function

--- src/common/getClient.ts
+++ src/common/getClient.ts
@@ -1,7 +1,10 @@
 import { MongoClient } from 'mongodb'
 
-export default async function (connectionString) {
-	const client = new MongoClient(connectionString, { useNewUrlParser: true })
+export default async function (connectionString, poolSize = 20) {
+	const client = new MongoClient(connectionString, {
+		useNewUrlParser: true,
+		poolSize,
+	})
 
 	return client.connect()
 }
