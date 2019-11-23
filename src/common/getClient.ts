5501d11c7eac943997172662d1a8c43b1314fe4b
Ezeki Zibzibadze <ez@jok.io>
2019-11-23 17:28:54 +0400

    fix: set useUnifiedTopology to mongo client

--- src/common/getClient.ts
+++ src/common/getClient.ts
@@ -3,6 +3,7 @@ import { MongoClient } from 'mongodb'
 export default async function (connectionString, poolSize = 20) {
 	const client = new MongoClient(connectionString, {
 		useNewUrlParser: true,
+		useUnifiedTopology: true,
 		poolSize,
 	})
 
