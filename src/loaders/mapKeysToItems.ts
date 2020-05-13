87a5b29dc6afa5180923856820c5397927581e88
Ezeki Zibzibadze <ez@jok.io>
2020-05-13 11:04:23 +0400

    fix: make mapKeysToItems arguments readonly

--- src/loaders/mapKeysToItems.ts
+++ src/loaders/mapKeysToItems.ts
@@ -1,5 +1,9 @@
 import { DocumentBase } from '../types'
 
-export const mapKeysToItems = (keys: string[]) => <T extends DocumentBase>(items: T[]) => {
-	return keys.map(id => <T>items.find(x => x.id === id) || null)
+export const mapKeysToItems = (keys: readonly string[]) => <
+	T extends DocumentBase
+>(
+	items: T[],
+) => {
+	return keys.map((id) => <T>items.find((x) => x.id === id) || null)
 }
